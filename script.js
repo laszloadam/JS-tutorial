78a9273e95ee4efa2ed4dd23cefeb48a2d24ae6e
László Ádám <laszloadam08@gmail.com>
2021-08-21 15:03:14 +0200

    fifth

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let test = document.getElementById('test');
+let result = document.getElementById('result');
 let form = document.getElementById('form');
 let questions = document.getElementsByClassName('questions');
-var actualValue = ""
+let ratingTitle = document.getElementById('ratingTitle');
+let ratingText = document.getElementById('ratingText');
+let win = 0;
+let fail = 0;
+var actualValue = "";
 let answers = [3,1,2];
 var serial = 1;
-
-test.innerHTML = actualValue;
+let thisQ = '';
 
 function selectQ(x) {
   actualValue = x.value;
@@ -24,26 +28,56 @@ function remHover(t){
   t.classList.remove("hover")
 }
 
+
 function nextQ(x){
     if(actualValue == answers[serial-1]){
-      let thisQ = document.getElementById('q'+serial+'_'+actualValue+'Label');
-      thisQ.style.backgroundColor = "green"
-    };
-    if (actualValue != answers[serial-1]) {
+      thisQ = document.getElementById('q'+serial+'_'+actualValue+'Label');
+      thisQ.style.backgroundColor = "green";
+      win++;
+      result.innerHTML = 'Win: '+win+' || Fail: '+fail;
+    }
+    else {
+      thisQ = document.getElementById('q'+serial+'_'+actualValue+'Label');
       thisQ.style.backgroundColor = "red";
+      fail++;
+      result.innerHTML = 'Win: '+win+' || Fail: '+fail;
     };
-    console.log('q'+serial+'_'+actualValue+'Label');
+    document.getElementById('qBtn'+serial).setAttribute('onclick', 'scrolling('+x+')')
 };
 
+
 function scrolling(x) {
-  document.getElementById('question'+x).scrollIntoView({behavior: "smooth"})
+  document.getElementById('question'+x).scrollIntoView({behavior: "smooth"});
+  serial=x;
+
 };
+
+function rating() {
+if(win==answers.length){
+  ratingTitle.innerHTML='Gratulálunk!';
+  ratingText.innerHTML='Eltaláltad az összes választ!';
+  }
+  else if(win!=answers.length){
+    ratingTitle.innerHTML='Gratulálunk!';
+    ratingText.innerHTML='Sajnos nem találtad el az összes helyes választ, de újra kezdheted, hogy kijavítsd őket.';
+    result.innerHTML = 'Helyes válaszok: '+win+' <br> Helytelen válaszok: '+fail;
+  }
+  else{
+    ratingTitle.innerHTML=':('
+    ratingText.innerHTML='Sajnos egy helyes választ sem találtál el. Kezdd elölről, hogy kijavítsd őket.'
+  }
+}
 
 function resetAll(){
   form.scrollTo(0,0);
-  scrollSlider.style.top = '-20px';
   serial=1;
-  for(let i=0; i<=questions.length; i++){
-    questions[i].style.backgroundColor="#909090"
+  win=0;
+  fail=0;
+  for(let x=1; x<=answers.length; x++){
+    document.getElementById('qBtn'+x).setAttribute('onclick', 'nextQ('+(x+1)+')')
+  };
+  for(let i=1; i<=answers.length; i++){
+    questions[i].style.backgroundColor="#909090";
+    document.getElementById('qBtn'+i).disabled=true;
     };
 }
