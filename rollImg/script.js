c1848cb7c0feb8c703c9ebb6e4ad8a231be93491
László Ádám <laszloadam08@gmail.com>
2021-08-23 22:38:28 +0200

    0823

--- rollImg/script.js
+++ rollImg/script.js
@@ -2,14 +2,36 @@ let images = ['dog.jpg', 'afternoon.jpg', 'lori.jpg', 'boardwalk.jpg']
 
 let fullView = document.getElementById('fullView');
 let one = document.getElementById('one');
-let two = document.getElementById('fullView');
+let two = document.getElementById('two');
 let three = document.getElementById('three');
+let minis = document.getElementById('minis');
+let miniView = document.getElementsByClassName('miniView');
+let picsArray=[];
 
 fullView.setAttribute('src', "./pics/"+images[1]);
-one.setAttribute('src', "./pics/"+images[0]);
-two.setAttribute('src', "./pics/"+images[1]);
-three.setAttribute('src', "./pics/"+images[2]);
+// one.setAttribute('src', "./pics/"+images[0]);
+// two.setAttribute('src', "./pics/"+images[1]);
+// three.setAttribute('src', "./pics/"+images[2]);
 
-function select(x) {
-    fullView.setAttribute('src', './pics/'+images[x])
+function imgList () {
+for (let i=0; i<images.length; i++){
+    picsArray.push('<div onclick="select('+i+')"><img class="miniView" src="./pics/'+images[i]+ '" alt="miniView"></div>');
+    }
+    minis.innerHTML=picsArray
+};
+
+
+function rightStep(){
+for(let x=0; x<=miniView.length; x++){
+    miniView.setAttribute('left', '-160px');
 }
+    picsArray.push(picsArray.shift(0));
+    
+    minis.innerHTML=picsArray
+}
+
+imgList()
+
+// function select(x) {
+//     fullView.setAttribute('src', './pics/'+images[x])
+// }
