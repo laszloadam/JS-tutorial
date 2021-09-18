f29cfcb043ef387d11c262f21ad2e6531e5a1b56
laszloadam <laszloadam08@gmail.com>
2021-09-18 16:32:28 +0200

    upgrade menu system

--- verticalTimeLineDNA/script.js
+++ verticalTimeLineDNA/script.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+console.log('Hello');
+let a = document.getElementsByTagName('a');
+console.log(a[0])
+
+function showIcon(x) {
+  a[x].style.display="block"
+}
+function hiddenIcon(x) {
+  a[x].style.display="none"
+}
